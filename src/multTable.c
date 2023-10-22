fb83e172dbbe1d10394be1bb06f12bbd97663b1f
saady-1122 <saadkhan1162003@gmail.com>
2023-10-22 06:57:10 -0700

    MultiTable.c modified

--- src/multTable.c
+++ src/multTable.c
@@ -1,6 +1,6 @@
 /**
- * Author: 
- * Date: 
+ * Author: Saad khan
+ * Date:10/22/2023 
  *
  * This program generates a multiplication table
  * of the size given as a command line argument.
@@ -23,5 +23,12 @@ int main(int argc, char **argv) {
 
   //TODO: place your code here
 
+  // ANSWER
+
+  Printf("Multiplication Table for %d:\n",n);
+  for (int i = 1; i <= 10; ++i){
+    printf("%d x %d = %d\n", n,i,n * 1);
+  }
   return 0;
+
 }
