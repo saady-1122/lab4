41e5d08f8b2caa76b00a4892cf82c0c50f2e483f
saady-1122 <saadkhan1162003@gmail.com>
2023-10-22 07:02:32 -0700

    output added to multTable.c

--- src/multTable.c
+++ src/multTable.c
@@ -25,10 +25,31 @@ int main(int argc, char **argv) {
 
   // ANSWER
 
-  Printf("Multiplication Table for %d:\n",n);
+  printf("Multiplication Table for %d:\n",n);
   for (int i = 1; i <= 10; ++i){
     printf("%d x %d = %d\n", n,i,n * 1);
   }
   return 0;
 
 }
+/*
+
+Output 
+
+PS E:\labwrk\CSCE155-C-Lab04\src> gcc .\multTable.c
+PS E:\labwrk\CSCE155-C-Lab04\src> .\a.exe
+Error: provide an integer
+PS E:\labwrk\CSCE155-C-Lab04\src> .\a.exe 100
+Multiplication Table for 100:
+100 x 1 = 100
+100 x 2 = 100
+100 x 3 = 100
+100 x 4 = 100
+100 x 5 = 100
+100 x 6 = 100
+100 x 7 = 100
+100 x 8 = 100
+100 x 9 = 100
+100 x 10 = 100
+
+*/
