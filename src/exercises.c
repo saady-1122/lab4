78b7ff066efdd05d441d8a68743eb757ccbe90be
saady-1122 <saadkhan1162003@gmail.com>
2023-10-22 05:19:00 -0700

    Question no 1 solved

--- src/exercises.c
+++ src/exercises.c
@@ -30,6 +30,22 @@ int main(int argc, char **argv) {
   }
   printf("\n");
 
+  ANSWER 
+
+  int i=10;
+  while (i<20){
+    printf("%d " ,i);
+    i +=3
+  }
+  printf("\n");
+
+
+
+
+
+
+
+
   //2. Rewrite the given while loop as an equivalent for loop
   int k = 3;
   while(k > -4) {
